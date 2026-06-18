c85d243616b60765b2526f321a8c918cb3be948a
hexlem <derpakarsen@gmail.com>
2026-06-18 11:00:26 +0300

    Add gun, basic ragdoll

---
+++ Assets/Scripts/ShootingWeapon.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ShootingWeapon : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletSpeed = 20f;
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        rb.linearVelocity = firePoint.forward * bulletSpeed;
+    }
+}
